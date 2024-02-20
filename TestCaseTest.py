6107f8b6fd8a57adeebcd9f83d147de28fb82499
sangsulee <sangsulee@gmarket.com>
2024-02-20 14:41:25 +0900

    ch19: TestCaseTest.py: add `testSetUp`

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -9,6 +9,12 @@ class TestCaseTest(TestCase.TestCase):
         test.run()
         assert (test.wasRun)
 
+    def testSetUp(self):
+        test = WasRun.WasRun("testMethod")
+        test.run()
+        assert (test.wasSetUp)
+
 
 if __name__ == '__main__':
     TestCaseTest("testRunning").testRunning()
+    TestCaseTest("TestSetUp").run()
