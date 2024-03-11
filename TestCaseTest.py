d8f655282fc5e4f6161409b94063d67c79e4af75
sangsulee <sangsulee@gmarket.com>
2024-03-12 08:43:40 +0900

    ch23: 모든 테스트 코드를 호출 하는 부분

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -31,6 +31,7 @@ class TestCaseTest(TestCase):
 
 
 if __name__ == '__main__':
-    TestCaseTest("testTemplateMethod").run()
-    TestCaseTest("testResult").run()
-    TestCaseTest("testFailedResult").run()
+    print(TestCaseTest("testTemplateMethod").run().summary())
+    print(TestCaseTest("testResult").run().summary())
+    print(TestCaseTest("testFailedResult").run().summary())
+    print(TestCaseTest("testFailedResultFormatting").run().summary())
